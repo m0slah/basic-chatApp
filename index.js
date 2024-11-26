b1300615e484c9c1a6fae4b10efd17d24a44321f
m0slah <muhammedslahqadir@gmail.com>
2024-11-26 04:27:08 +0300

    create connection

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+io.on("connection", (socket) => {
+  console.log("a client connected ✅");
+
+  socket.on("chat message", (message) => {
+    io.emit("chat message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a client disconnected");
+  });
+});
+
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
